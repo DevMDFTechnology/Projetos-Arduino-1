ed126ee875e408e7a065e4549da68eca6f0bb1f0
DevMDFTechnology <desenvolvimento@mdftechnology.com.br>
2024-07-25 09:35:18 -0300

    V2.0.1 09-Receptor-RS485.cpp

--- 09-Receptor-RS485.cpp
+++ 09-Receptor-RS485.cpp
@@ -1,43 +1,65 @@
 #include <SoftwareSerial.h>
 
-#define Pino_RS485_RX 10 // DI do Módulo RS485
-#define Pino_RS485_TX 11 // DO do Módulo RS485
-#define SSerialTxControl 3 
-
+#define Pino_RS485_RX 10 // DO (Direct Output)
+#define Pino_RS485_TX 11 // DI (Direct Input)
+#define SSerialTxControl 3
 #define RS485Transmit HIGH
 #define RS485Receive LOW
 
 SoftwareSerial RS485Serial(Pino_RS485_RX, Pino_RS485_TX);
 
-String dadosSerial = "";
+String inputString = "";
+unsigned long previousMillis = 0; // Armazena o último tempo em que a função bufferPrint foi chamada
+const unsigned long interval = 500; // Intervalo fixo de 500 ms para todas as chamadas
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Modulo Transmissor");
-  Serial.println("Digite um Valor na Serial para enviar os dados...");
+  Serial.println("Módulo Receptor");
+  Serial.println("Aguardando dados...");
   pinMode(SSerialTxControl, OUTPUT);
+  digitalWrite(SSerialTxControl, RS485Receive);
   RS485Serial.begin(4800);
 }
 
 void loop() {
-  while (Serial.available() > 0) {
-    char c = Serial.read();
-    dadosSerial += c;
-    
-    // Se encontrar um caractere de nova linha ('\n'), termina a entrada
-    if (c == '\n') {
-      // Envia os dados no formato solicitado: /003011X,/001011X,/002022X,
-      dadosSerial.replace("R", "X");
-      enviarDados(dadosSerial); // Envia os dados acumulados
-      dadosSerial = ""; // Limpa a string para a próxima entrada
+  if (RS485Serial.available()) {
+    while (RS485Serial.available()) {
+      char inChar = (char)RS485Serial.read();
+      inputString += inChar;
+      if (inChar == '\n') {
+        processInput(inputString);
+        inputString = "";
+      }
     }
+  }
+
+  // Chama bufferPrint em intervalos regulares
+  if (millis() - previousMillis >= interval) {
+    previousMillis = millis();
+    bufferPrint();
   }
 }
 
-void enviarDados(String dados) {
-  digitalWrite(SSerialTxControl, RS485Transmit);
-  RS485Serial.println(dados); // Envia os dados pela RS485
-  RS485Serial.flush(); // Garante que todos os dados sejam enviados
-  digitalWrite(SSerialTxControl, RS485Receive);
-  Serial.println("Dados enviados: " + dados); // Exibe os dados enviados via Serial monitor
+void processInput(String input) {
+  input.trim();
+  input.replace("/", "");
+  int startPos = 0;
+  int commaPos = input.indexOf(',', startPos);
+  while (commaPos != -1) {
+    String codigo = input.substring(startPos, commaPos);
+    bufferPrint(codigo);
+    startPos = commaPos + 1;
+    commaPos = input.indexOf(',', startPos);
+  }
+}
+
+void bufferPrint(String codigo = "") {
+  if (codigo.length() > 0) {
+    codigo.trim();
+    codigo.replace("X", "R");
+    codigo.replace("x", "r");
+    
+    // Imprime o código
+    Serial.println(codigo + ",");
+  }
 }
